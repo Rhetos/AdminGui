5286140eb12e7534efa8da3ed5b77b115292a34a
HuyNguyen <nguyenquanghuy031295@gmail.com>
2018-09-20 17:11:03 +0700

    use another method to download Rhetos v2.0.0

--- Tools/AdminGuiBuildCore.psm1
+++ Tools/AdminGuiBuildCore.psm1
@@ -1,25 +1,25 @@
 
-$DatabaseName="TestAdminGui"
+$DatabaseName = "TestAdminGui"
 Export-ModuleMember -Variable 'DatabaseName'
-$Port="9000"
+$Port = "9000"
 Export-ModuleMember -Variable 'Port'
-$SQLServer="(local)"
+$SQLServer = "(local)"
 Export-ModuleMember -Variable 'SQLServer'
 
-function Install-RhetosServer($rhetosVersion="2.0.0") {
-    $url = "http://github.com//Rhetos/Rhetos/releases/download/v$rhetosVersion/Rhetos.$rhetosVersion.zip"
+function Install-RhetosServer($rhetosVersion = "2.0.0") {
+    $uri = "http://github.com//Rhetos/Rhetos/releases/download/v$rhetosVersion/Rhetos.$rhetosVersion.zip"
     $zipDstPath = ".\Rhetos.$rhetosVersion.zip"
     $dstPath = ".\2CS.RhetosBuild"
     if (!(Test-Path $dstPath)) {
         if (!(Test-Path $zipDstPath)) {
             [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
             Try {
-                Write-Verbose "Downloading Rhetos......."
-                $wc = New-Object System.Net.WebClient
-                $wc.DownloadFile($url, $zipDstPath)
+                Write-Host "Downloading Rhetos......."
+                Invoke-WebRequest -Uri $uri -OutFile $zipDstPath
             }
             Catch {
                 Write-Error "$($error[0])"
+                throw
             }
         
         }
@@ -48,10 +48,10 @@ function Initialize-RhetosServer($sqlServer, $databaseName) {
 }
 
 function New-PluginBinaryDirectories() {
-    If(!(Test-Path ".\RhetosPackages\Plugins")) {
+    If (!(Test-Path ".\RhetosPackages\Plugins")) {
         New-Item -ItemType directory -Path ".\RhetosPackages\Plugins"
     }
-    If(!(Test-Path ".\RhetosPackages\BasecodePlugins")) {
+    If (!(Test-Path ".\RhetosPackages\BasecodePlugins")) {
         New-Item -ItemType directory -Path ".\RhetosPackages\BasecodePlugins"
     }
 }
@@ -66,7 +66,8 @@ function Build-Frontend() {
     cd AdminGUI
     if (!(Test-Path ".\node_modules")) {
         npm install
-    } else {
+    }
+    else {
         npm update
     }
     Write-Host "*** AdminGui NPM PACKAGES UPDATED. ***" -ForegroundColor Green
@@ -80,8 +81,7 @@ function Build-Frontend() {
     cd ..
 }
 
-function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
-{
+function RegexReplace ($fileSearch, $replacePattern, $replaceWith) {
     Get-ChildItem $fileSearch -r | % {
         $c = [IO.File]::ReadAllText($_.FullName, [System.Text.Encoding]::Default) -Replace $replacePattern, $replaceWith;
         [IO.File]::WriteAllText($_.FullName, $c, [System.Text.Encoding]::UTF8)
@@ -89,28 +89,30 @@ function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
 }
 
 function Change-Version($buildVersion, $prereleaseVersion) {
-    If($prereleaseVersion -eq "auto"){
+    If ($prereleaseVersion -eq "auto") {
         $prereleaseText = ('A' + (get-date -format "yyMMddHHmm") + '-' + (git rev-parse --short HEAD))
-        If($prereleaseText.length -gt 20) {
-            $prereleaseVersion = $prereleaseText.Substring(0,20)
-        } Else {
-            $prereleaseVersion = $prereleaseText.Substring(0,$prereleaseText.length)
+        If ($prereleaseText.length -gt 20) {
+            $prereleaseVersion = $prereleaseText.Substring(0, 20)
+        }
+        Else {
+            $prereleaseVersion = $prereleaseText.Substring(0, $prereleaseText.length)
         }
     } 
 
     If (![string]::IsNullOrEmpty($prereleaseVersion)) {
         $fullVersion = $buildVersion + '-' + $prereleaseVersion
-    } Else {
+    }
+    Else {
         $fullVersion = $buildVersion
     }
 
     Write-Output "Setting version $fullVersion"
     cd AdminGui
-    RegexReplace ".\*.nuspec" '([\n^]\s*\<version\>).*(\<\/version\>\s*)' ('${1}'+$fullVersion+'${2}')
+    RegexReplace ".\*.nuspec" '([\n^]\s*\<version\>).*(\<\/version\>\s*)' ('${1}' + $fullVersion + '${2}')
     cd ..
 }
 
-function New-NugetPackages($buildVersion="1.0.0", $prereleaseVersion="auto") {
+function New-NugetPackages($buildVersion = "1.0.0", $prereleaseVersion = "auto") {
     try {
         Change-Version $buildVersion $prereleaseVersion
         nuget pack AdminGui\Rhetos.AdminGui.nuspec -OutputDirectory .
@@ -129,7 +131,7 @@ function Update-RhetosServer() {
     try {
         cd 2CS.RhetosBuild\Rhetos\bin\
         .\DeployPackages.exe /NOPAUSE
-        If(Test-Path ".\Plugins\AdminSetup.exe") {
+        If (Test-Path ".\Plugins\AdminSetup.exe") {
             .\Plugins\AdminSetup.exe
         }
         cd ..\..\..\
