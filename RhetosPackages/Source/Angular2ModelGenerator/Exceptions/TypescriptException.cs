5a95edf7a63ad68c648e87b9546bf70968125f50
HuyNguyen <nguyenquanghuy031295@gmail.com>
2018-10-05 14:28:34 +0700

    remove "tsc" globally in plugin

---
+++ RhetosPackages/Source/Angular2ModelGenerator/Exceptions/TypescriptException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angular2ModelGenerator.Exceptions
+{
+    public class TypeScriptException : Exception
+    {
+        public TypeScriptException(string message) : base(message)
+        {
+        }
+    }
+}
