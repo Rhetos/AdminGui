f8894a3b6535bf15597f637da9be3331c1fc637b
Lam Le <15520405@gm.uit.edu.vn>
2018-10-12 11:39:36 +0700

    Lazy catching errors after all commands
    Commands should throw exceptions internally but for now, and I also think it does not hurt if we double-check.

--- Init.ps1
+++ Init.ps1
@@ -15,13 +15,16 @@ Process {
 
         Write-Verbose "Download and install Rhetos server."
         Install-RhetosServer
-
+		if (!$?) { throw }
+		
         Write-Verbose "Initialize default configurations for the testing Rhetos server."
         Initialize-RhetosServer $SQLServer $DatabaseName
-
+		if (!$?) { throw }
+		
         Write-Verbose "Prepare directories required by the projects."
         New-PluginBinaryDirectories
-
+		if (!$?) { throw }
+		
         Pop-Location
     }
     catch {
